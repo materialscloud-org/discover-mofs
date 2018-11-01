dc3cd5a62708a038e4b0b703fbdf17719d8cf131
Leopold Talirz <leopold.talirz@gmail.com>
2018-11-01 11:36:39 +0100

    update download script

--- prepare.sh
+++ prepare.sh
@@ -3,13 +3,14 @@
 cd data/
 
 # Download data
-export base_url=http://archive.materialscloud.org/file/2018.0003/v2; \
-    wget ${base_url}/structures.tgz &&\
-    wget ${base_url}/properties.tgz
+export base_url=https://archive.materialscloud.org/preview_file/afa9ec805767a4da2bdaf4abf774f2ba163d74540d06318c; \
+    wget ${base_url}/MOF_database.tar.gz &&\
+    wget ${base_url}/screening_data.tar.gz
 
 # Extract data
-tar xf structures.tgz && rm structures.tgz && \
-    tar xf properties.tgz && rm properties.tgz
+mkdir -p structures
+tar xf MOF_database.tar.gz -C structures/ && rm MOF_database.tar.gz && \
+    tar xf screening_data.tar.gz  && rm screening_data.tar.gz
 
 cd ..
 
