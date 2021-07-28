3371d7374def844489510aec1f181b8029c94159
Leopold Talirz <leopold.talirz@gmail.com>
2021-07-28 09:41:57 +0200

    fix security vulnerability

--- setup.py
+++ setup.py
@@ -11,12 +11,12 @@ if __name__ == '__main__':
         name="discover-mofs",
         author="Leopold Talirz",
         author_email="info@materialscloud.org",
-        description="bokeh application for COF discover section.",
+        description="bokeh application for MOF discover section.",
         license="MIT",
         classifiers=["Programming Language :: Python"],
         version="0.1.0",
         install_requires=[
             "bokeh~=1.3.4", "jsmol-bokeh-extension~=0.2.1", "pandas~=1.3.1",
-            "sqlalchemy~=1.0.19", "ruamel.yaml~=0.16.5", "requests~=2.26.0"
+            "sqlalchemy~=1.3.0", "ruamel.yaml~=0.16.5", "requests~=2.26.0"
         ],
         extras_require={"pre-commit": ["pre-commit~=2.2.0", "pylint~=2.6.0"]})
